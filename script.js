06719dd896d7b4c5383230cfe6a9afae4736814b
nthBit <207577365+nthBit@users.noreply.github.com>
2026-05-21 17:23:21 +0100

    Create dynamic grid area logic
    - Add grid reset button
    - Add prompt to allow user choice
    - Allow values between 1 and 100 (inclusive)

--- script.js
+++ script.js
@@ -3,14 +3,31 @@ const divContainer = document.createElement('div');
 divContainer.id = 'divContainer';
 
 function createDynamicGrid(num) {
-    for (let i = 0; i < num; i++) {
+    for (let i = 0; i < (num ** 2); i++) {
         const div = document.createElement('div');
         div.addEventListener('mouseover', () => {
             div.style.backgroundColor = "black";
         });
+        div.style.width = `${800 / num}px`;
+        div.style.height = `${800 / num}px`;
         divContainer.appendChild(div);
         body.appendChild(divContainer);
     }
 }
 
-createDynamicGrid(256);
+const btn = document.createElement('button');
+btn.textContent = 'Reset Grid';
+body.appendChild(btn);
+
+btn.addEventListener('click', () => {
+    while (true) {
+        gridRows = prompt('Choose: 1 ≤ num ≤ 100.');
+        if (1 <= gridRows && gridRows <= 100) {
+            break;
+        } else {
+            continue;
+        }
+    }
+    divContainer.replaceChildren();
+    createDynamicGrid(gridRows);
+});
